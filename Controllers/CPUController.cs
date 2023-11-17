7a309bab77d9a3e39951fe9d3dcb0ddfc7b6c90e
H8YOU212 <slavyan.212@mail.ru>
2023-11-17 12:41:29 +0500

    CpuController

---
+++ Controllers/CPUController.cs
@@ -0,0 +1,52 @@
+﻿using comp_company.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace comp_company.Controllers
+{
+    public class CPUController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All")]
+        public IActionResult GetAll()
+        {
+            CompContext compContext = new CompContext();
+            return Ok(compContext.Cpus);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Cpu cpu)
+        {
+            CompContext compContext = new CompContext();
+            compContext.Cpus.Add(cpu);
+            compContext.SaveChanges();
+            return Ok(cpu);
+        }
+        [HttpGet]
+        public IActionResult GetId(int Id)
+        {
+            CompContext compContext = new CompContext();
+            Cpu? cpu = compContext.Cpus.FirstOrDefault(x => x.Id == Id);
+            if (cpu == null) { return NotFound(); }
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Update(Cpu cpu)
+        {
+            CompContext compContext = new CompContext(); 
+            compContext.Cpus.Update(cpu);
+            return Ok(cpu);
+        }
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            CompContext compContext = new CompContext();
+            Cpu? cpu = compContext.Cpus.FirstOrDefault(x => x.Id == Id);
+            if (Id == null) { return NotFound(); }
+            compContext.Remove(cpu);
+            compContext.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
