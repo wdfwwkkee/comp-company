7e8e8ad2d9b521d4098a36fca37d6fdf014005da
wdfwwkkee <denis.usmanov101@mail.ru>
2023-11-17 12:41:45 +0500

    gpuController

---
+++ Controllers/GPUController.cs
@@ -0,0 +1,30 @@
+﻿using comp_company.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace comp_company.Controllers
+{
+
+    [ApiController]
+    [Route("GPU")]
+    public class GPUController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All")]
+        public ActionResult All()
+        {
+            CompContext compContext = new CompContext();
+            return Ok(compContext.Gpus);
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+
+        public ActionResult Delete()
+        {
+            return Ok();
+        }
+        
+
+    }
+}
