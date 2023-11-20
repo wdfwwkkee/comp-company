7b38fd3a6ed73db5988e730f75c89b55ec8b8664
H8YOU212 <slavyan.212@mail.ru>
2023-11-20 09:04:34 +0500

    Fix client

---
+++ CompCompanyClients/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace CompCompanyClients.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ CompCompanyClients/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace CompCompanyClients.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
